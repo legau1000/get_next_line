ea91a1ca7cbdac57c4b03c3e2c0ee48c7e59e6fb
Florian <florian.gau@epitech.eu>
2018-01-08 18:27:24 +0100

    If read_size if <= 0

--- get_next_line.c
+++ get_next_line.c
@@ -29,7 +29,7 @@ char *read_mo(int fd, char *buff, int *err, int *size)
 {
 	free(buff);
 	buff = malloc(READ_SIZE + 1);
-	if (buff) {
+	if (READ_SIZE >= 0 && buff) {
 		*err = read(fd, buff, READ_SIZE);
 		if (*err == -1)
 			return (NULL);
@@ -50,19 +50,28 @@ char *good_or_no(int *nb_byte_read, char *tab, int *index_tab)
 	return (tab);
 }
 
+char *adding_value_on_tab(char *tab, char *buff, int *index_tab, int *where_i_am)
+{
+	tab[*index_tab] = buff[*where_i_am];
+	*index_tab = *index_tab + 1;
+	*where_i_am = *where_i_am + 1;
+	return (tab);
+}
+
 char *get_next_line(int fd)
 {
 	int index_tab = 0;
 	int index_nb_read = 1;
 	char *tab = malloc(index_nb_read * READ_SIZE);
 
-	if (nb_byte_read == -2)
+	if (nb_byte_read == -2) {
 		buff = read_mo(fd, buff, &nb_byte_read, &index_nb_read);
+		if (!buff)
+			return (NULL);
+	}
 	while (buff && nb_byte_read != 0 && buff[where_i_am] != '\n') {
 		if (where_i_am < nb_byte_read) {
-			tab[index_tab] = buff[where_i_am];
-			index_tab++;
-			where_i_am++;
+			tab = adding_value_on_tab(tab, buff, &index_tab, &where_i_am);
 		} else {
 			buff = read_mo(fd, buff, &nb_byte_read, &index_nb_read);
 			tab = paste_strings(tab, index_nb_read);
